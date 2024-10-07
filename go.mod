476bcc87efa4d209236ab40e1b9dd98cfb2b5ac6
StutenEXE <alexandre.bidaux78@gmail.com>
2024-10-07 17:36:30 +0200

    Initial commit (y'a R qui fonctionne le S)

---
+++ go.mod
@@ -0,0 +1,3 @@
+module td5
+
+go 1.23.1
---
+++ types.go
@@ -0,0 +1,15 @@
+package td5
+
+type BallotRequest struct {
+	// Operator string `json:"op"`
+	// Args     [2]int `json:"args"`
+}
+
+type VoteRequest struct {
+	// Operator string `json:"op"`
+	// Args     [2]int `json:"args"`
+}
+
+type Response struct {
+	Result int `json:"res"`
+}
